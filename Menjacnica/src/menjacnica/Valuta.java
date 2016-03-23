d2b7a86b701117e5cc6cb8779cd5f06e07bb7a39
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-03-23 17:12:56 +0100

    Napravljene klasa Valuta(sa atributima naziv i skraceniNaziv) i klasa Kurs (sa atributima prodajni, kupovni, srednji, datum). Svi atributi imaju odgovarajuce get i set metode.

---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,23 @@
+package menjacnica;
+
+public class Valuta {
+	private String nazivValute;
+	private String skraceniNazivValute;
+
+	public String getNazivValute() {
+		return nazivValute;
+	}
+
+	public void setNazivValute(String nazivValute) {
+		this.nazivValute = nazivValute;
+	}
+
+	public String getSkraceniNazivValute() {
+		return skraceniNazivValute;
+	}
+
+	public void setSkraceniNazivValute(String skraceniNazivValute) {
+		this.skraceniNazivValute = skraceniNazivValute;
+	}
+
+}
